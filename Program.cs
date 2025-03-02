e63f7d0b9ed3b44db544406bf056310454515855
5053241007_Arya Raka Firmansyah <aryarakaf2005@gmail.com>
2025-03-02 23:01:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿//Nama : Arya Raka Firmansyah
+//NRP : 5053241007
+//Matkul : Pemrograman Berorientasi Objek
+// Tugas : Mmebuat Hello World
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Raka");
