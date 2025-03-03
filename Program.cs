0daa6d7a21654bce7ab3984d89276e3987d6f60a
5053241007_Arya Raka Firmansyah <aryarakaf2005@gmail.com>
2025-03-03 13:24:51 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,13 +2,5 @@
 //NRP : 5053241007
 //Matkul : Pemrograman Berorientasi Objek
 //Tugas : Membuat Hello World
-
-using System;
-
-class Program{
-  static void Main()
-  {
-  Console.WriteLine("Hello, World!");
-  Console.WriteLine("My name is Raka");
-  }
-}
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Raka");
